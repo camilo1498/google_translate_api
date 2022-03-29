bcd37428becca3f7a1c601f9a31561ffb884a435
camilo velandia <davidcamilo96000@gmail.com>
2022-03-29 09:39:12 -0500

    deleted unuseful dependencies

--- server.js
+++ server.js
@@ -2,12 +2,10 @@ const express = require('express');
 const app = express();
 const http = require('http');
 const server = http.createServer(app);
-const cors = require('cors');
 
 const port = process.env.PORT || 3000;
 const routes = require('./routes');
 
-app.use(cors());
 routes(app);
 
 server.listen(port, function() {
